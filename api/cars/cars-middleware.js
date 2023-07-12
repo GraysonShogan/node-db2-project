5ab0b4933ab015063a577429b92cbb259bbcdfbf
Grayson Shogan <graysontoddshogan@gmail.com>
2023-07-12 12:56:59 -0700

    wip

--- api/cars/cars-middleware.js
+++ api/cars/cars-middleware.js
@@ -1,15 +1,55 @@
-const checkCarId = (req, res, next) => {
-  // DO YOUR MAGIC
+const Cars = require("./cars-model");
+
+async function checkCarId(req, res, next) {
+  const { id } = req.params;
+  try {
+    const car = await Cars.getById(id);
+    if (car) {
+      next();
+    } else {
+      res.status(404).json({ message: `Car with id ${id} is not found` });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error" });
+  }
 }
 
-const checkCarPayload = (req, res, next) => {
-  // DO YOUR MAGIC
+function checkCarPayload(req, res, next) {
+  const { vin, make, model, mileage } = req.body;
+  if (!vin || !make || !model || !mileage) {
+    res.status(400).json({ message: "Required field(s) are missing" });
+  } else {
+    next();
+  }
 }
 
-const checkVinNumberValid = (req, res, next) => {
-  // DO YOUR MAGIC
+function checkVinNumberValid(req, res, next) {
+  const { vin } = req.body;
+  const vinRegex = /^[A-HJ-NPR-Z0-9]{17}$/;
+  if (!vinRegex.test(vin)) {
+    res.status(400).json({ message: `VIN ${vin} is invalid` });
+  } else {
+    next();
+  }
 }
 
-const checkVinNumberUnique = (req, res, next) => {
-  // DO YOUR MAGIC
+async function checkVinNumberUnique(req, res, next) {
+  const { vin } = req.body;
+  try {
+    const car = await db("cars").where({ vin }).first();
+    if (car) {
+      res.status(400).json({ message: `VIN ${vin} already exists` });
+    } else {
+      next();
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error" });
+  }
 }
+
+module.exports = {
+  checkCarId,
+  checkCarPayload,
+  checkVinNumberValid,
+  checkVinNumberUnique,
+};
--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,50 @@
-// DO YOUR MAGIC
+const express = require("express");
+const Cars = require("./cars-model");
+const {
+  checkCarId,
+  checkCarPayload,
+  checkVinNumberValid,
+  checkVinNumberUnique,
+} = require("./cars-middleware");
+
+const router = express.Router();
+
+// [GET] /api/cars
+router.get("/", async (req, res) => {
+  try {
+    const cars = await Cars.getAll();
+    res.json(cars);
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+// [GET] /api/cars/:id
+router.get("/:id", checkCarId, async (req, res) => {
+  const { id } = req.params;
+  try {
+    const car = await Cars.getById(id);
+    res.json(car);
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+// [POST] /api/cars
+router.post(
+  "/",
+  checkCarPayload,
+  checkVinNumberValid,
+  checkVinNumberUnique,
+  async (req, res) => {
+    const car = req.body;
+    try {
+      const newCar = await Cars.create(car);
+      res.status(201).json(newCar);
+    } catch (error) {
+      res.status(500).json({ message: "Internal server error" });
+    }
+  }
+);
+
+module.exports = router;
