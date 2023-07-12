e49f30d8fd782bbfaf9f01f796fb019f5e450cab
Grayson Shogan <graysontoddshogan@gmail.com>
2023-07-12 12:36:27 -0700

    wip

--- api/server.js
+++ api/server.js
@@ -1,7 +1,55 @@
-const express = require("express")
+const express = require("express");
+const knex = require("knex");
+const dbConfig = require("./knexfile");
 
-const server = express()
+const app = express();
 
-// DO YOUR MAGIC
+const db = knex(dbConfig.development);
 
-module.exports = server
+app.use(express.json());
+
+app.get("/cars", async (req, res) => {
+  try {
+    const cars = await db("cars");
+    res.json(cars);
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+app.get("/cars/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const car = await db("cars").where({ id }).first();
+    if (car) {
+      res.json(car);
+    } else {
+      res.status(404).json({ error: "Car not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+app.post("/cars", async (req, res) => {
+  const { vin, make, model, mileage, title, transmission } = req.body;
+  if (!vin || !make || !model || !mileage) {
+    res.status(400).json({ error: "Missing required fields" });
+    return;
+  }
+  try {
+    const newCar = await db("cars").insert({
+      vin,
+      make,
+      model,
+      mileage,
+      title,
+      transmission,
+    });
+    res.json({ id: newCar[0], ...req.body });
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+module.exports = server;
